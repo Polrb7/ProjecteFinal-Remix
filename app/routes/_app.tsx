891ea2c7bfb84237b4dcbcd7ca8e53a1a0666e08
PolRB <polrb7@gmail.com>
2025-01-12 18:41:31 +0100

    final commit

--- app/routes/_app.tsx
+++ app/routes/_app.tsx
@@ -1,6 +1,7 @@
 import { Outlet } from '@remix-run/react';
 import Footer from '~/components/navs/Footer';
 import Header from '~/components/navs/Header';
+// YOOW
 
 export default function MainPageLayout(): JSX.Element {
 	return (
