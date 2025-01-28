44ef5e149cf97ba3c7d04dc24c6cb7570bb7948a
PolRB <polrb7@gmail.com>
2025-01-28 18:03:54 +0100

    applying filters attempts + yow

--- app/routes/_app.index.tsx
+++ app/routes/_app.index.tsx
@@ -1,7 +1,7 @@
 import { LoaderFunctionArgs } from "@remix-run/node";
 import { getAllBooks } from "~/data/books";
 import { getAllUsers } from "~/data/users";
-import { getAllLikes, deleteLike, addLike } from "~/data/likes";
+import { getAllLikes } from "~/data/likes";
 import { getAuthToken, getLoggedUser } from "~/data/auth";
 import { Book, User, Like } from "~/types/interfaces";
 import { useLoaderData, Form, Link } from "@remix-run/react";
@@ -14,8 +14,6 @@ export async function loader({ request }: LoaderFunctionArgs): Promise<{ books: 
   const userLogged = await getLoggedUser(request);
   const userLoggedId = userLogged.id;
   const userLoggedRole = userLogged.admin;
-  console.log(userLoggedId);
-  
 
   if (!authToken) {
     throw new Response("Unauthorized", { status: 401 });
@@ -28,12 +26,11 @@ export async function loader({ request }: LoaderFunctionArgs): Promise<{ books: 
 }
 
 export default function BookList() {
-  const { books, users, likes: initialLikes, userLoggedId, userLoggedRole, authToken } = useLoaderData<{ books: Book[], users: User[], likes: Like[], userLoggedId: number | null, userLoggedRole: boolean, authToken: string }>();
+  const { books, users, likes: initialLikes, userLoggedId, userLoggedRole } = useLoaderData<{ books: Book[], users: User[], likes: Like[], userLoggedId: number | null, userLoggedRole: boolean }>();
   const [likes, setLikes] = useState<Like[]>(initialLikes);
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [selectedBookId, setSelectedBookId] = useState<number | null>(null);
 
-  // Modal per confirmar la eliminació d'un llibre
   const openModal = (bookId: number) => {
     setSelectedBookId(bookId);
     setIsModalOpen(true);
@@ -51,164 +48,178 @@ export default function BookList() {
     closeModal();
   };
 
-  // Estat per al text del cercador
+  // Estat per al text del cercador i gènere
   const [searchText, setSearchText] = useState("");
   const [selectedGenre, setSelectedGenre] = useState("");
 
-  // Llibres filtrats segons el text del cercador i el gènere seleccionat
-  const filteredBooks = books.filter((book) =>
-    book.title.toLowerCase().includes(searchText.toLowerCase()) &&
-    (selectedGenre === "" || book.genre === selectedGenre)
-  );
+  // Filtrar llibres al fer clic al botó
+  const [filteredBooks, setFilteredBooks] = useState(books);
+
+  const applyFilter = () => {
+    console.log("Apply filter clicked"); // Depuració
+    const newFilteredBooks = books.filter((book) =>
+      book.title.toLowerCase().includes(searchText.toLowerCase()) &&
+      (selectedGenre === "" || book.genre === selectedGenre)
+    );
+    console.log("Filtered books:", newFilteredBooks); // Depuració
+    setFilteredBooks(newFilteredBooks);
+  };
+  
 
   // Obtenir tots els gèneres únics
   const genres = Array.from(new Set(books.map((book) => book.genre)));
 
   return (
     <>
-    <div className="max-w-6xl mx-auto mt-10 p-6">
-    <div className="flex justify-end">
-      <Link
-      to={`/viewProfile/${userLoggedId}`}
-      className="text-black hover:scale-105 hover:bg-gray-300 hover:font-semibold pl-5 hover:px-3 hover:py-2 hover:rounded text-md font-medium transition-colors"
-      >
-      Profile
-      </Link>
-    </div>
-      <h1 className="text-3xl font-bold mb-6 text-primaryBlack-default dark:text-primaryYellow-default">
-        Book List
-      </h1>
-
-      <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-4 mb-6">
-        {/* Input del cercador */}
-        <input
-          type="text"
-          name="buscador"
-          id="buscador"
-          placeholder="Search..."
-          className="flex-1 p-2 border border-gray-300 rounded"
-          value={searchText}
-          onChange={(e) => setSearchText(e.target.value)} // Actualitza l'estat amb el text introduït
-        />
-
-        {/* Dropdown per seleccionar el gènere */}
-        <div className="flex-1 flex items-center">
-          <label htmlFor="genre">Genre: </label>
-          <select
-            name="genre"
-            id="genre"
-            className="flex-1 p-2 border border-gray-300 rounded"
-            value={selectedGenre}
-            onChange={(e) => setSelectedGenre(e.target.value)} // Actualitza l'estat amb el gènere seleccionat
+      <div className="max-w-6xl mx-auto mt-10 p-6">
+        <div className="flex justify-end">
+          <Link
+            to={`/viewProfile/${userLoggedId}`}
+            className="text-black hover:scale-105 hover:bg-gray-300 hover:font-semibold pl-5 hover:px-3 hover:py-2 hover:rounded text-md font-medium transition-colors"
           >
-            <option value="">All Genres</option>
-            {genres.map((genre) => (
-              <option key={genre} value={genre}>
-                {genre}
-              </option>
-            ))}
-          </select>
+            Profile
+          </Link>
         </div>
-        
-      </div>
+        <h1 className="text-3xl font-bold mb-6 text-primaryBlack-default dark:text-primaryYellow-default">
+          Book List
+        </h1>
 
-      {/* Llista de llibres filtrats */}
-      <div id="llista" className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-        {filteredBooks.map((book: Book) => {
-          const user = users.find((user) => user.id === book.user_id);
-          const userLike = likes.find((like) => like.book_id === book.id && like.user_id === userLoggedId);
-          return (
-            <div
-              id="book"
-              key={book.id}
-              className="bg-white p-4 rounded-lg shadow-md"
+        <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-4 mb-6">
+          {/* Input del cercador */}
+          <input
+            type="text"
+            name="buscador"
+            id="buscador"
+            placeholder="Search..."
+            className="flex-1 p-2 border border-gray-300 rounded"
+            value={searchText}
+            onChange={(e) => setSearchText(e.target.value)}
+          />
+
+          {/* Dropdown per seleccionar el gènere */}
+          <div className="flex-1 flex items-center">
+            <label htmlFor="genre">Genre: </label>
+            <select
+              name="genre"
+              id="genre"
+              className="flex-1 p-2 border border-gray-300 rounded"
+              value={selectedGenre}
+              onChange={(e) => setSelectedGenre(e.target.value)}
             >
-              <Link to={`/books/details/${book.id}`}>
-                {book.book_img ? (
-                  <img
-                    src={book.book_img}
-                    alt={book.title}
-                    className="w-full h-40 object-cover rounded-lg mb-4" />
-                ) : (
-                  <div className="w-full h-40 bg-gray-200 rounded-lg flex items-center justify-center text-gray-500">
-                    No Image
+              <option value="">All Genres</option>
+              {genres.map((genre) => (
+                <option key={genre} value={genre}>
+                  {genre}
+                </option>
+              ))}
+            </select>
+          </div>
+
+          {/* Botó per aplicar el filtratge */}
+          <button
+            className="bg-blue-500 text-white px-4 py-2 rounded"
+            onClick={applyFilter}
+          >
+            Filter
+          </button>
+        </div>
+
+        {/* Llista de llibres filtrats */}
+        <div id="llista" className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+          {filteredBooks.map((book: Book) => {
+            const user = users.find((user) => user.id === book.user_id);
+            const userLike = likes.find((like) => like.book_id === book.id && like.user_id === userLoggedId);
+            return (
+              <div
+                id="book"
+                key={book.id}
+                className="bg-white p-4 rounded-lg shadow-md"
+              >
+                <Link to={`/books/details/${book.id}`}>
+                  {book.book_img ? (
+                    <img
+                      src={book.book_img}
+                      alt={book.title}
+                      className="w-full h-40 object-cover rounded-lg mb-4"
+                    />
+                  ) : (
+                    <div className="w-full h-40 bg-gray-200 rounded-lg flex items-center justify-center text-gray-500">
+                      No Image
+                    </div>
+                  )}
+                  <h2 id="title" className="text-xl font-bold mb-2 text-primaryBlack-default dark:text-primaryWhite-default">
+                    {book.title}
+                  </h2>
+                  <p className="text-sm text-primaryWhite-default mb-2">
+                    Genre: {book.genre}
+                  </p>
+                  <div className="flex justify-between items-center">
+                    <span className="text-sm text-primaryYellow-default font-bold">
+                      Author:
+                    </span>
+                    <span className="text-primaryBlack-default dark:text-primaryWhite-default">
+                      {book.author}
+                    </span>
                   </div>
-                )}
-                <h2 id="title" className="text-xl font-bold mb-2 text-primaryBlack-default dark:text-primaryWhite-default">
-                  {book.title}
-                </h2>
-                <p className="text-sm text-primaryWhite-default mb-2">
-                  Genre: {book.genre}
-                </p>
-                <div className="flex justify-between items-center">
-                  <span className="text-sm text-primaryYellow-default font-bold">
-                    Author:
-                  </span>
-                  <span className="text-primaryBlack-default dark:text-primaryWhite-default">
-                    {book.author}
-                  </span>
-                </div>
-                <div className="mt-4 text-right">
-                  <span className="text-sm italic text-gray-500">
-                    Uploaded on {new Date(book.created_at).toLocaleDateString()}
-                  </span>
-                  {user && (
-                    <span className="text-sm italic text-gray-500"> by {user.username}</span>
+                  <div className="mt-4 text-right">
+                    <span className="text-sm italic text-gray-500">
+                      Uploaded on {new Date(book.created_at).toLocaleDateString()}
+                    </span>
+                    {user && (
+                      <span className="text-sm italic text-gray-500"> by {user.username}</span>
+                    )}
+                  </div>
+                </Link>
+                <div className="mt-4 flex justify-evenly flex-col space-y-2 md:flex-row md:space-y-0 md:space-x-2">
+                  <button className="bg-blue-700 text-white px-4 py-2 rounded">
+                    <Link to={`/books/details/${book.id}`}>View Details</Link>
+                  </button>
+
+                  {(userLoggedId === book.user_id || userLoggedRole) && (
+                    <>
+                      <button className="bg-yellow-700 text-white px-4 py-2 rounded">
+                        <Link to={`/books/edit/${book.id}`}>Edit</Link>
+                      </button>
+                      <Form method="post" action={`/books/delete/${book.id}`} id={`delete-form-${book.id}`}>
+                        <button
+                          type="button"
+                          className="bg-red-600 w-full text-white px-4 py-2 rounded"
+                          onClick={() => openModal(book.id)}
+                        >
+                          Delete
+                        </button>
+                      </Form>
+                    </>
                   )}
                 </div>
-              </Link>
-              <div className="mt-4 flex justify-evenly flex-col space-y-2 md:flex-row md:space-y-0 md:space-x-2">
-                <button className="bg-blue-700 text-white px-4 py-2 rounded">
-                  <Link to={`/books/details/${book.id}`}>View Details</Link>
-                </button>
-
-                {(userLoggedId === book.user_id || userLoggedRole) && (
-                  <>
-                    <button className="bg-yellow-700 text-white px-4 py-2 rounded">
-                      <Link to={`/books/edit/${book.id}`}>Edit</Link>
-                    </button>
-                    <Form method="post" action={`/books/delete/${book.id}`} id={`delete-form-${book.id}`}>
-                      <button
-                        type="button"
-                        className="bg-red-600 w-full text-white px-4 py-2 rounded"
-                        onClick={() => openModal(book.id)}
-                      >
-                        Delete
+                <div className="mt-4 flex justify-center">
+                  {userLike ? (
+                    <Form method="post" action={`/likes/delete/${userLike.id}`}>
+                      <button aria-label="like" type="submit">
+                        <FaHeart className={"text-red-500 cursor-pointer"} />
                       </button>
                     </Form>
-                  </>
-                )}
+                  ) : (
+                    <Form method="post" action={`/likes/add/${book.id}`}>
+                      <button aria-label="like" type="submit">
+                        <FaHeart className={"text-gray-400 cursor-pointer"} />
+                      </button>
+                    </Form>
+                  )}
+                </div>
               </div>
-              <div className="mt-4 flex justify-center">
-                {userLike ? (
-                  <Form method="post" action={`/likes/delete/${userLike.id}`}>
-                    <button aria-label="like" type="submit">
-                      <FaHeart
-                        className={"text-red-500 cursor-pointer"} />
-                    </button>
-                  </Form>
-                ) : (
-                  <Form method="post" action={`/likes/add/${book.id}`}>
-                    <button aria-label="like" type="submit">
-                      <FaHeart
-                        className={"text-gray-400 cursor-pointer"} />
-                    </button>
-                  </Form>
-                )}
-              </div>
-            </div>
-          );
-        })}
+            );
+          })}
+        </div>
+        <Modal
+          isOpen={isModalOpen}
+          onClose={closeModal}
+          onConfirm={confirmDelete}
+          title="Confirm Deletion"
+        >
+          Are you sure you want to delete this book?
+        </Modal>
       </div>
-      <Modal
-        isOpen={isModalOpen}
-        onClose={closeModal}
-        onConfirm={confirmDelete}
-        title="Confirm Deletion"
-      >
-        Are you sure you want to delete this book?
-      </Modal>
-    </div>
     </>
   );
 }
--- app/routes/_app.viewUsers.tsx
+++ app/routes/_app.viewUsers.tsx
@@ -2,6 +2,7 @@ import { Form, useLoaderData } from "@remix-run/react";
 import { getAllUsers, deleteUser, updateUser } from "~/data/users";
 import { User } from "~/types/interfaces";
 import { getAuthToken } from "~/data/auth";
+import { useState } from "react";
 
 export async function loader({ request }: { request: Request }) {
   const authToken = await getAuthToken(request);
@@ -42,54 +43,86 @@ export async function action({ request }: { request: Request }) {
 }
 
 export default function ViewUsers() {
-  const users = useLoaderData<User[]>();
+  const users = useLoaderData<User[]>(); // Dades dels usuaris carregades
+  const [searchResults, setSearchResults] = useState("");
+
+  // Filtrar usuaris pel nom i cognom
+  const filteredUsers = users.filter((user) => {
+    const name = user.name || "";
+    const surname = user.surname || "";
+
+    const fullName = `${name} ${surname}`.toLowerCase();
+    return fullName.includes(searchResults.toLowerCase());
+  });
 
   return (
-    <div className="container mx-auto p-8">
-      <h1 className="text-3xl font-bold mb-6">Users</h1>
-      <div className="overflow-x-auto">
-        <table className="min-w-full bg-white">
-          <thead>
-            <tr>
-              <th className="py-2 px-4 border-b">ID</th>
-              <th className="py-2 px-4 border-b">Name</th>
-              <th className="py-2 px-4 border-b">Surname</th>
-              <th className="py-2 px-4 border-b">Username</th>
-              <th className="py-2 px-4 border-b">Email</th>
-              <th className="py-2 px-4 border-b">Age</th>
-              <th className="py-2 px-4 border-b">Actions</th>
-            </tr>
-          </thead>
-          <tbody>
-            {users.map((user) => (
-              <tr key={user.id}>
-                <td className="py-2 text-center px-4 border-b">{user.id}</td>
-                <td className="py-2 text-center px-4 border-b">{user.name}</td>
-                <td className="py-2 text-center px-4 border-b">{user.surname}</td>
-                <td className="py-2 text-center px-4 border-b">{user.username}</td>
-                <td className="py-2 text-center px-4 border-b">{user.email}</td>
-                <td className="py-2 text-center px-4 border-b">{user.age}</td>
-                <td className="py-2 flex flex-col lg:flex-row gap-3 text-center px-4 border-b">
-                  <Form method="post">
-                    <input type="hidden" name="userId" value={user.id} />
-                    <input type="hidden" name="actionType" value={user.admin ? "demote" : "ascend"} />
-                    <button type="submit" className={`w-full text-white px-4 py-2 rounded ${user.admin ? "bg-yellow-700" : "bg-blue-600"}`}>
-                      {user.admin ? "Demote" : "Ascend"}
-                    </button>
-                  </Form>
-                  <Form method="post">
-                    <input type="hidden" name="userId" value={user.id} />
-                    <input type="hidden" name="actionType" value="delete" />
-                    <button type="submit" className="w-full bg-red-600 text-white px-4 py-2 rounded">
-                      Delete
-                    </button>
-                  </Form>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
+    <>
+      {/* Input de cerca */}
+      <div className="relative w-full mb-4">
+        <input
+          id="search"
+          type="text"
+          placeholder="Search users..."
+          className="w-full p-2 pl-10 text-lg border border-gray-300 rounded"
+          value={searchResults}
+          onChange={(e) => setSearchResults(e.target.value)}
+        />
       </div>
-    </div>
+
+      {/* Taula d'usuaris */}
+      <div className="container mx-auto p-8">
+        <h1 className="text-3xl font-bold mb-6">Users</h1>
+        <div className="overflow-x-auto">
+          <table className="min-w-full bg-white">
+            <thead>
+              <tr>
+                <th className="py-2 px-4 border-b">ID</th>
+                <th className="py-2 px-4 border-b">Name</th>
+                <th className="py-2 px-4 border-b">Surname</th>
+                <th className="py-2 px-4 border-b">Username</th>
+                <th className="py-2 px-4 border-b">Email</th>
+                <th className="py-2 px-4 border-b">Age</th>
+                <th className="py-2 px-4 border-b">Actions</th>
+              </tr>
+            </thead>
+            <tbody>
+              {filteredUsers.length === 0 && (
+                <tr>
+                  <td colSpan={7} className="py-2 text-center px-4 border-b">
+                    No users found
+                  </td>
+                </tr>
+              )}
+              {filteredUsers.map((user) => (
+                <tr key={user.id}>
+                  <td className="py-2 text-center px-4 border-b">{user.id}</td>
+                  <td className="py-2 text-center px-4 border-b">{user.name}</td>
+                  <td className="py-2 text-center px-4 border-b">{user.surname}</td>
+                  <td className="py-2 text-center px-4 border-b">{user.username}</td>
+                  <td className="py-2 text-center px-4 border-b">{user.email}</td>
+                  <td className="py-2 text-center px-4 border-b">{user.age}</td>
+                  <td className="py-2 flex flex-col lg:flex-row gap-3 text-center px-4 border-b">
+                    <Form method="post">
+                      <input type="hidden" name="userId" value={user.id} />
+                      <input type="hidden" name="actionType" value={user.admin ? "demote" : "ascend"} />
+                      <button type="submit" className={`w-full text-white px-4 py-2 rounded ${user.admin ? "bg-yellow-700" : "bg-blue-600"}`}>
+                        {user.admin ? "Demote" : "Ascend"}
+                      </button>
+                    </Form>
+                    <Form method="post">
+                      <input type="hidden" name="userId" value={user.id} />
+                      <input type="hidden" name="actionType" value="delete" />
+                      <button type="submit" className="w-full bg-red-600 text-white px-4 py-2 rounded">
+                        Delete
+                      </button>
+                    </Form>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </>
   );
 }
