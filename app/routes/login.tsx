e7c41d509a772abaabc5896883ff641a1f5c6727
PolRB <polrb7@gmail.com>
2025-02-06 18:28:30 +0100

    header fix attempt + login redirection fix attempt (both failed) + yoow

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -1,39 +1,38 @@
-import { ActionFunctionArgs } from '@remix-run/node'
-import LoginForm from '../components/auth/LoginForm'
-import { validateLoginCredentials } from '~/data/validations'
-import { login } from '../data/auth'
+import { ActionFunctionArgs } from '@remix-run/node';
+import LoginForm from '../components/auth/LoginForm';
+import { validateLoginCredentials } from '~/data/validations';
+import { login } from '../data/auth';
 
 export default function Index() {
-	return (
-		<div>
-			<LoginForm />
-		</div>
-	)
+  return (
+    <div>
+      <LoginForm />
+    </div>
+  );
 }
 
 export async function action({ request }: ActionFunctionArgs) {
-	const formData = await request.formData()
-	const email = formData.get('email') as string
-	const password = formData.get('password') as string
+  const formData = await request.formData();
+  const email = formData.get('email') as string;
+  const password = formData.get('password') as string;
 
-	if (!email || !password) {
-		return {
-			validationErrors: {
-				email: 'Email is required',
-				password: 'Password is required',
-			},
-			status: 400,
-		}
-	}
+  if (!email || !password) {
+    return {
+      validationErrors: {
+        email: 'Email is required',
+        password: 'Password is required',
+      },
+      status: 400,
+    };
+  }
 
-	if (validateLoginCredentials({ email, password })) {
-		return validateLoginCredentials({ email, password })
-	}
+  if (validateLoginCredentials({ email, password })) {
+    return validateLoginCredentials({ email, password });
+  }
 
-	try {
-		return await login({ email, password })
-	} catch (error: unknown) {
-		// console.error('Error during authentication:', error)
-		return error
-	}
+  try {
+    return await login({ email, password });
+  } catch (error: unknown) {
+    return error;
+  }
 }
